e61e19f166a33b2b628d2daadc126a9ce8b76af0
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-10-08 20:42:01 -0500

    finished fish db model

--- models/fish.js
+++ models/fish.js
@@ -1,100 +1,76 @@
 module.exports = function (sequelize, DataTypes) {
   var fish = sequelize.define("fish", {
     common_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-        len: [1],
-    },
-    scientific_name: {
       type: DataTypes.TEXT,
       allowNull: false,
       len: [1],
     },
+    scientific_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1],
+    },
     max_size: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      len: [1],
     },
     temp_low: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      len: [1],
     },
     temp_high: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      len: [1],
     },
     ph_low: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      len: [1],
     },
     ph_high: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      len: [1],
     },
     min_tank: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      len: [1],
     },
     aggressive: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      len: [1],
     },
     schooling: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      len: [1],
+    },
+    min_group: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      len: [1],
     },
     ph_high: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
-    },ph_high: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
-    },ph_high: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1],
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      len: [1],
+    },
+    tank_level: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1],
+    },
+    lifespan: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      len: [1],
     },
   });
 
-  // fish.associate = function(models) {
-  //   // We're saying that a Post should belong to an Author
-  //   // A Post can't be created without an Author due to the foreign key constraint
-  //   fish.belongsTo(models.Author, {
-  //     foreignKey: {
-  //       allowNull: false
-  //     }
-  //   });
-  // };
-
   return fish;
 };
-
-
-    common_name text NOT NULL,
-    scientific_name varchar
-    (100) NOT NULL,
-    max_size float 
-    NOT NULL,
-    temp_low FLOAT NOT NULL,
-    temp_high FLOAT NOT NULL,
-    ph_low FLOAT NOT NULL,
-    ph_high FLOAT NOT NULL,
-    min_tank FLOAT NOT NULL,
-    aggressive BOOLEAN,
-    schooling BOOLEAN,
-    min_group int NOT NULL,
-    tank_level varchar
-    (50),
-    lifespan int NOT NULL,
-    PRIMARY KEY
-    (id)
-);
