9ffe0aacf6f3b932efd1c7880a96e5860488cfef
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-10-13 12:43:22 -0500

    form.js and form.html

---
+++ public/js/form.js
@@ -0,0 +1,6 @@
+$("#button").on("click", function () {
+    event.preventDefault()
+    var inputOne = $("#input1").val()
+    var inputTwo = $("#input2").val()
+    console.log(inputOne, inputTwo)
+})
